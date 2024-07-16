eb57ba15d9094b91310dc321130872072e731235
BentlyM <metayerbently@gmail.com>
2024-07-16 16:12:20 -0400

    added loader

--- src/components/home/WeatherApp.js
+++ src/components/home/WeatherApp.js
@@ -1,8 +1,26 @@
 const displayWeatherCardContent = () => {
     
-    const weatherCardContent = document.createElement('p');
-    weatherCardContent.textContent = 'Weather API content will be displayed here';
-    
+    const weatherCardContent = document.createElement('div');
+    weatherCardContent.classList.add('weather-content');
+
+    const loaderDisplay = () => {
+      const loadingWarp = document.createElement('div');
+      loadingWarp.classList.add('load-warp');
+
+      const loader = document.createElement('div');
+      loader.id = 'loader';
+
+      const loaderText = document.createElement('p');
+      loaderText.textContent = 'Weather API content will be displayed here';
+
+      loadingWarp.appendChild(loader);
+      loadingWarp.appendChild(loaderText);
+
+      return loadingWarp;
+    }
+
+
+    weatherCardContent.appendChild(loaderDisplay());
     return weatherCardContent;
   };
   
